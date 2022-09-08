77a7c52daa767d51ceede85eaf38a8254523c083
sachieko <arashikabane@gmail.com>
2022-09-07 19:52:12 -0600

    Daylist and daylistitems finished

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classNames from "classnames";
+import 'components/DayListItem.scss'
+
+export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item',
+   { 
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots 
+    });
+
+  const Spots = (props) => {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>
+    }
+    return props.spots === 1 ? <h3 className="text--light">{props.spots} spot remaining</h3> : <h3 className="text--light">{props.spots} spots remaining</h3>;
+  };
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <Spots spots={props.spots} />
+      
+    </li>
+  );
+};
