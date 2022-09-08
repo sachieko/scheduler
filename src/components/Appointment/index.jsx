dad4c07321e90d036f1b40f7ea3c748cea56f93e
sachieko <arashikabane@gmail.com>
2022-09-08 01:20:33 -0600

    Blank appointment components render

---
+++ src/components/Appointment/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import 'components/Appointment/styles.scss';
+
+const Appointment = (props) => {
+  return (
+    <article className="appointment">
+
+    </article>
+  )
+};
+
+export default Appointment;
