c1eabd6abb23a3aa4633ab9f8d151ed2bb8fe381
sachieko <arashikabane@gmail.com>
2022-09-08 21:57:39 -0600

    Add form component with props

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 storiesOf('Button', module)
   .addParameters({
@@ -170,4 +171,6 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message={"Deleting"} />)
-  .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />);
+  .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
+  .add("Create Form", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Edit Form", () => <Form student="Sachieko" interviewer={interviewer.id} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />);
