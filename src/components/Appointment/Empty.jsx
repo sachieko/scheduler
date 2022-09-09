bcaaab6792210fc40aa9db80acbe3d2b3ec3e80c
sachieko <arashikabane@gmail.com>
2022-09-08 21:11:03 -0600

    Add header and empty components for appointments

---
+++ src/components/Appointment/Empty.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Empty = (props) => {
+  return (
+  <main onClick={props.onAdd} className="appointment__add">
+    <img
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+    />
+  </main>
+  )
+};
+
+export default Empty;
