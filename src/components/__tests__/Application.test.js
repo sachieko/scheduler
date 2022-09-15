108c0b80c0ab983966ef794bc6b7ef2ca2414386
sachieko <arashikabane@gmail.com>
2022-09-15 01:39:51 -0600

    Implement more tests for application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, cleanup, waitForElement, fireEvent, queryByText,
-   getAllByRole, getByAltText, getByPlaceholderText, getByText, findByText, findAllByText } from '@testing-library/react';
+import { render, cleanup, waitForElement, fireEvent, queryByText, findByAltText,
+   getAllByRole, getByAltText, getByPlaceholderText, getByText, findByText, findAllByText, findByPlaceholderText } from '@testing-library/react';
 import Application from 'components/Application';
+import axios from 'axios';
 
 afterEach(cleanup);
 
@@ -22,19 +23,155 @@ describe('Application tests', () => {
 
     const firstAppointment = getAllByRole(container, 'article')[0];
     const firstAddButton = getByAltText(firstAppointment, 'Add');
+
     fireEvent.click(firstAddButton);
     expect(queryByText(firstAppointment, 'Sachieko Test')).not.toBeInTheDocument();
+
     fireEvent.change(getByPlaceholderText(firstAppointment, /Enter Student Name/i), {
       target: { value: 'Sachieko Test' }
     });
+
     const firstInterviewer = getAllByRole(firstAppointment, 'img')[0];
     fireEvent.click(firstInterviewer);
     expect(getByPlaceholderText(firstAppointment, 'Sachieko Test')).toHaveValue('Sachieko Test');
+
     fireEvent.click(getByText(firstAppointment, 'Save'));
     expect(getByText(firstAppointment, 'Saving...')).toBeInTheDocument();
+    
     const studentName = await findByText(firstAppointment, 'Sachieko Test');
     expect(studentName).toBeInTheDocument();
+    
     const spots = await findAllByText(container, 'spot', { exact: false })
     expect(getByText(spots[0], 'no spots remaining', {exact: false})).toBeInTheDocument();
+  });
+  it('loads data, cancels an interview, and increases the spots remaining for the first day by 1 to 2', async() => {
+    const  { container } = render(<Application />);
+
+    await waitForElement(() => container);
+
+    const secondBookedAppointment = getAllByRole(container, 'article')[1];
+    const deleteButton = getByAltText(secondBookedAppointment, 'Delete');
+    
+    expect(getByText(secondBookedAppointment, 'Archie Cohen')).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+    expect(getByText(secondBookedAppointment, 'Delete the appointment?')).toBeInTheDocument();
+    
+    fireEvent.click(getByText(secondBookedAppointment, 'Confirm'));
+    expect(getByText(secondBookedAppointment, 'Deleting...')).toBeInTheDocument();
+
+    const addButton = await findByAltText(secondBookedAppointment, 'Add');
+    expect(addButton).toBeInTheDocument();
+
+    const spots = await findAllByText(container, 'spot', { exact: false })
+    expect(getByText(spots[0], '2 spots remaining', {exact: false})).toBeInTheDocument();
+  });
+  it('allows users to edit an interview and keep spots on Monday remaining the same', async() => {
+    const  { container } = render(<Application />);
+
+    await waitForElement(() => container);
+
+    const secondBookedAppointment = getAllByRole(container, 'article')[1];
+    const editButton = getByAltText(secondBookedAppointment, 'Edit');
+    
+    expect(getByText(secondBookedAppointment, 'Archie Cohen')).toBeInTheDocument();
+
+    fireEvent.click(editButton);
+
+    fireEvent.change(getByPlaceholderText(secondBookedAppointment, /Archie Cohen/i), {
+      target: { value: 'The Rat Prince' }
+    });
+
+    fireEvent.click(getByText(secondBookedAppointment, 'Save'));
+    expect(getByText(secondBookedAppointment, 'Saving...')).toBeInTheDocument();
+    
+    const studentName = await findByText(secondBookedAppointment, 'The Rat Prince');
+    expect(studentName).toBeInTheDocument();
+    
+    const spots = await findAllByText(container, 'spot', { exact: false })
+    expect(getByText(spots[0], '1 spot remaining', {exact: false})).toBeInTheDocument();
+    
+  });
+  it('shows the save error when failing to save an appointment', async() => {
+    axios.put.mockRejectedValueOnce();
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => container);
+
+    const firstAppointment = getAllByRole(container, 'article')[0];
+    const firstAddButton = getByAltText(firstAppointment, 'Add');
+
+    fireEvent.click(firstAddButton);
+    expect(queryByText(firstAppointment, 'The Rat Prince')).not.toBeInTheDocument();
+
+    fireEvent.change(getByPlaceholderText(firstAppointment, /Enter Student Name/i), {
+      target: { value: 'The Rat Prince' }
+    });
+
+    const firstInterviewer = getAllByRole(firstAppointment, 'img')[0];
+    fireEvent.click(firstInterviewer);
+    expect(getByPlaceholderText(firstAppointment, 'The Rat Prince')).toBeInTheDocument();
+
+    fireEvent.click(getByText(firstAppointment, 'Save'));
+    expect(getByText(firstAppointment, 'Saving...')).toBeInTheDocument();
+    
+    const errorMsg = await findByText(firstAppointment, 'There was an error while saving the appointment, please try again.');
+    expect(errorMsg).toBeInTheDocument();
+
+    fireEvent.click(getByAltText(firstAppointment, 'Close'));
+    const emptyInput = await findByPlaceholderText(firstAppointment, /Enter Student Name/i);
+    expect(emptyInput).toBeInTheDocument();
+
+    const spots = await findAllByText(container, 'spot', { exact: false })
+    expect(getByText(spots[0], '1 spot remaining', {exact: false})).toBeInTheDocument();
+
+  });
+  it('shows the delete error when failing to delete an appointment', async() => {
+    axios.delete.mockRejectedValueOnce();
+    const  { container } = render(<Application />);
+
+    await waitForElement(() => container);
+
+    const secondBookedAppointment = getAllByRole(container, 'article')[1];
+    const deleteButton = getByAltText(secondBookedAppointment, 'Delete');
+    
+    expect(getByText(secondBookedAppointment, 'Archie Cohen')).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+    expect(getByText(secondBookedAppointment, 'Delete the appointment?')).toBeInTheDocument();
+    expect(queryByText(secondBookedAppointment, 'Archie Cohen')).not.toBeInTheDocument();
+    
+    fireEvent.click(getByText(secondBookedAppointment, 'Confirm'));
+    expect(queryByText(secondBookedAppointment, 'Deleting...')).toBeInTheDocument();
+
+    const errorMsg = await findByText(secondBookedAppointment, 'There was an error while deleting the appointment, please try again.');
+    expect(errorMsg).toBeInTheDocument();
+
+    fireEvent.click(getByAltText(secondBookedAppointment, 'Close'));
+    const sameAppointment = await findByText(secondBookedAppointment, 'Archie Cohen');
+    expect(sameAppointment).toBeInTheDocument();
+  });
+  it('Does not change an appointment when a user begins to delete an interview but clicks cancel', async() => {
+    const  { container } = render(<Application />);
+
+    await waitForElement(() => container);
+
+    const secondBookedAppointment = getAllByRole(container, 'article')[1];
+    const deleteButton = getByAltText(secondBookedAppointment, 'Delete');
+    
+    expect(getByText(secondBookedAppointment, 'Archie Cohen')).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+    expect(getByText(secondBookedAppointment, 'Delete the appointment?')).toBeInTheDocument();
+    expect(queryByText(secondBookedAppointment, 'Archie Cohen')).not.toBeInTheDocument();
+    
+    fireEvent.click(getByText(secondBookedAppointment, 'Cancel'));
+    expect(queryByText(secondBookedAppointment, 'Deleting...')).not.toBeInTheDocument();
+
+    const studentName = getByText(secondBookedAppointment, 'Archie Cohen');
+    expect(studentName).toBeInTheDocument();
+
+    const spots = await findAllByText(container, 'spot', { exact: false })
+    expect(getByText(spots[0], '1 spot remaining', {exact: false})).toBeInTheDocument();
   });
 });
