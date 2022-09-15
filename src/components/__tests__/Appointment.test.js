0ca768d9fa7ad13e8a8ab8ab41ed8c2ce34c881a
sachieko <arashikabane@gmail.com>
2022-09-14 18:28:08 -0600

    Remove mock state

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -5,8 +5,6 @@ import Appointment from 'components/Appointment';
 
 afterEach(cleanup);
 
-const mockState = {};
-
 const interviewers = [
   { id: 1, name: 'Sylvia Palmer', avatar: 'https://i.imgur.com/LpaY82x.png' },
   { id: 2, name: 'Tori Malcolm', avatar: 'https://i.imgur.com/Nmx0Qxo.png' },
